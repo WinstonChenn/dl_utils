bc6b5491db8cadb4a45579e28e9df8dea6a7a2d8
winstonchenn <chenwt@uw.edu>
2021-06-05 19:06:04 -0700

    change default beta

--- train_utils.py
+++ train_utils.py
@@ -79,7 +79,7 @@ def get_model(device, num_classes, net_str, optim_type, model=None, lr=0.001,
 
 
 def train(checkpoint_dir, net, train_loader, vali_loader, net_str, data_label,
-          rho, device, criterion, optimizer, beta=1, epochs=1):
+          rho, device, criterion, optimizer, beta=0.0, epochs=1):
     losses = []
     vali_losses = []
     vali_accues = []
